1ef8054429bf4868ef6c776d2458c20e6885d075
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-02-22 12:37:51 +0100

    feat: refactor environment configuration and add Clerk environment variables

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import "./src/env.ts";
+import "./src/env";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,21 +1,16 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import z from "zod";
+import { env as clerkEnv } from "./clerk";
 
 export const env = createEnv({
+    extends: [clerkEnv],
+
     server: {
         NODE_ENV: z
             .enum(["development", "test", "production"])
             .default("development"),
         DATABASE_URL: z.url(),
-        CLERK_SECRET_KEY: z.string(),
     },
 
-    client: {
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-    },
-
-    experimental__runtimeEnv: {
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
-            process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    },
+    experimental__runtimeEnv: {},
 });
