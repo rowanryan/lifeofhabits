8e1f50fba5dd07fd592586794de68b0217945f01
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-02-22 12:48:06 +0100

    refactor: update environment configuration to separate Clerk variables and adjust DATABASE_URL type

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,15 +1,15 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import z from "zod";
-import { env as clerkEnv } from "./clerk";
+//import { env as clerkEnv } from "./extensions/clerk";
 
 export const env = createEnv({
-    extends: [clerkEnv],
+    //extends: [clerkEnv],
 
     server: {
         NODE_ENV: z
             .enum(["development", "test", "production"])
             .default("development"),
-        DATABASE_URL: z.url(),
+        DATABASE_URL: z.string(),
     },
 
     experimental__runtimeEnv: {},
