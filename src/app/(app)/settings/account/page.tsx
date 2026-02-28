dbbce00a829f2cd44a7ffb2f5e23997a0740a39e
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-02-28 20:54:12 +0100

    feat: conditionally render UserConnectedAccounts based on available OAuth providers for improved user experience

--- src/app/(app)/settings/account/page.tsx
+++ src/app/(app)/settings/account/page.tsx
@@ -1,13 +1,17 @@
+import { OAUTH_PROVIDERS } from "@/config/clerk";
 import { UserConnectedAccounts } from "@/features/user-connected-accounts";
 import { UserEmailAddresses } from "@/features/user-email-addresses";
 import { Profile } from "./components/Profile";
 
 export default function Page() {
+    const hasOAuthProviders = OAUTH_PROVIDERS.length > 0;
+
     return (
         <div className="space-y-4">
             <Profile />
             <UserEmailAddresses />
-            <UserConnectedAccounts />
+
+            {hasOAuthProviders && <UserConnectedAccounts />}
         </div>
     );
 }
