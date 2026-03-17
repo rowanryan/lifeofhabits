998a7894a1c74826dbb2cf3df50f42a0e91732f0
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-03-17 18:19:11 +0100

    refactor: remove billing-related components and API integrations for Polar

--- src/app/(app)/settings/billing/page.tsx
+++ src/app/(app)/settings/billing/page.tsx
@@ -1,28 +1,3 @@
-import { Subscription } from "./components/Subscription";
-import { getCustomerState, getInternalCustomer } from "./queries";
-
 export default async function Page() {
-    const internalCustomer = await getInternalCustomer();
-    const customerState = await getCustomerState();
-
-    const subscription =
-        customerState && internalCustomer.subscriptionId
-            ? {
-                  id: internalCustomer.subscriptionId,
-                  meter: customerState.meter,
-                  plan: customerState.plan,
-              }
-            : null;
-
-    return (
-        <div className="space-y-4">
-            <Subscription
-                subscription={subscription}
-                internalCustomer={{
-                    id: internalCustomer.id,
-                    spendLimit: internalCustomer.spendLimit,
-                }}
-            />
-        </div>
-    );
+    return null;
 }
--- src/lib/polar.ts
+++
@@ -1,7 +0,0 @@
-import { Polar } from "@polar-sh/sdk";
-import { env } from "@/env";
-
-export const api = new Polar({
-    accessToken: env.POLAR_ACCESS_TOKEN,
-    server: env.APP_ENV === "development" ? "sandbox" : "production",
-});
