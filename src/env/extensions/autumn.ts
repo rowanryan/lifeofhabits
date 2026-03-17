3eec1b429a862a7471e88a9d02904d1eabeaa8be
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-03-17 18:24:15 +0100

    feat: add autumn environment configuration and update env extension imports

---
+++ src/env/extensions/autumn.ts
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import z from "zod";
+
+export const env = createEnv({
+    server: {
+        AUTUMN_PROD_SECRET_KEY: z.string(),
+        AUTUMN_SECRET_KEY: z.string(),
+    },
+
+    client: {},
+
+    experimental__runtimeEnv: {},
+
+    skipValidation: process.env.NODE_ENV === "test",
+});
