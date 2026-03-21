2e2b41347eda01df99078699b046fcdfb492d433
Jonathan van de Groep <jrr.vdgroep@gmail.com>
2026-03-21 19:58:55 +0100

    fix: update default schedule value from weekday to day in CreateHabit component for improved clarity

--- src/features/habits/components/CreateHabit.tsx
+++ src/features/habits/components/CreateHabit.tsx
@@ -78,7 +78,7 @@ export function CreateHabit({ children, ...props }: CreateHabitProps) {
         defaultValues: {
             name: "",
             description: "",
-            schedule: defaultScheduleValues.weekday,
+            schedule: defaultScheduleValues.day,
         },
     });
 
